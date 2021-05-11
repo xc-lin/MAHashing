593393d4689d157373d16a35095445e2343c0bdb
xc-lin <736196315@qq.com>
2021-05-11 11:42:21 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/MAHashing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MAHashing.dir/bits.c.o"
+  "CMakeFiles/MAHashing.dir/buckets.c.o"
+  "MAHashing"
+  "MAHashing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MAHashing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
